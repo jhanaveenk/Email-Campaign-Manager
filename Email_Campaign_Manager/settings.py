66f8e331fd318c13413297a73351993cbbfbe603
Naveen Kumar Jha <71990959+jhanaveenk@users.noreply.github.com>
2023-09-09 18:22:48 +0530

    some files on directory added

--- Email_Campaign_Manager/settings.py
+++ Email_Campaign_Manager/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,7 +127,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 't5739319@gmail.com'
-EMAIL_HOST_PASSWORD = 'saqmkejvldgcmjxv'
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 
 # Default primary key field type
